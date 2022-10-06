0759afbedd9bc3737fffdea95c7491dcf500c73b
Andrea Debernardi <andrea@debbaweb.it>
2022-10-06 10:37:18 +0200

    add procedures for update password

---
+++ wp-toolkit/update_password.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, use: ./update_password.sh email password"
+    exit
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No argument supplied, use: ./update_password.sh email password"
+    exit
+fi
+
+EMAIL=$1
+PASSWORD=$2
+
+for DOMAIN_ID in $DOMAIN_IDS
+do
+
+      SITE_URL=$(plesk ext wp-toolkit --wp-cli -instance-id "$DOMAIN_ID" -- option get siteurl)
+
+      echo "Domain ID: $DOMAIN_ID"
+      echo "Website: $SITE_URL"
+
+      USERS=$(plesk ext wp-toolkit --wp-cli -instance-id "$DOMAIN_ID" -- user list --fields=user_email | sed 1d | grep $EMAIL)
+      USERS_COUNT=$(echo -n "$USERS" | grep -c '^')
+
+      if [ "$USERS_COUNT" -gt 0 ]; then
+            echo "Email trovata: $EMAIL, aggiorno la password"
+            plesk ext wp-toolkit --wp-cli -instance-id "$DOMAIN_ID" -- user update "$EMAIL" --user_pass="$PASSWORD" --skip-email
+      fi
+
+      echo "_____"
+done
